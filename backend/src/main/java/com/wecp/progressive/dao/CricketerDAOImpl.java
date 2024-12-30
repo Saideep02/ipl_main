6881ba25071f6e9a049e3c413e6432b17f805478
Saideep02 <saideep.balusa@ltimindtree.com>
2024-12-31 01:21:44 +0530

    Update CricketerDAOImpl.java

--- backend/src/main/java/com/wecp/progressive/dao/CricketerDAOImpl.java
+++ backend/src/main/java/com/wecp/progressive/dao/CricketerDAOImpl.java
@@ -4,29 +4,35 @@ import java.util.List;
 
 import com.wecp.progressive.entity.Cricketer;
 
-public class CricketerDAOImpl implements CricketerDAO{
+public class CricketerDAOImpl implements CricketerDAO {
 
     @Override
     public int addCricketer(Cricketer cricketer) {
-       return -1;
+    return -1;   
     }
 
     @Override
     public Cricketer getCricketerById(int cricketerId) {
-    return null;
-  }
+    return null;    
+    }
 
     @Override
     public void updateCricketer(Cricketer cricketer) {
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'updateCricketer'");
     }
 
     @Override
     public void deleteCricketer(int cricketerId) {
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'deleteCricketer'");
     }
 
     @Override
     public List<Cricketer> getAllCricketers() {
-    return List.of();
-  }
+        // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'getAllCricketers'");
+   return List.of();
+    }
 
 }
