8c1f6c70455d58a7b40381fa2e3c719d402d0dc8
username <wecp-dummy@gmail.com>
2025-01-03 12:51:04 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/controller/TeamController.java
+++ backend/src/main/java/com/wecp/progressive/controller/TeamController.java
@@ -2,40 +2,47 @@ package com.wecp.progressive.controller;
 
 import com.wecp.progressive.entity.Team;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 public class TeamController {
+    @GetMapping("/team")
     public ResponseEntity<List<Team>> getAllTeams() {
+        return ResponseEntity.ok(null);
+    }
+    @GetMapping("/team/{teamID}")
+    public ResponseEntity<Team> getTeamById(@PathVariable int teamID) {
         return null;
     }
-
-    public ResponseEntity<Team> getTeamById(int teamId) {
+    @PostMapping("/team")
+    public ResponseEntity<Integer> addTeam(@RequestBody Team team) {
         return null;
     }
-
-    public ResponseEntity<Integer> addTeam(Team team) {
+    @PutMapping("/team/{teamID}")
+    public ResponseEntity<Void> updateTeam(@PathVariable int teamID,@RequestBody Team team) {
         return null;
     }
-
-    public ResponseEntity<Void> updateTeam(int teamId, Team team) {
+    @DeleteMapping("/team/{teamID}")
+    public ResponseEntity<Void> deleteTeam(@PathVariable int teamID) {
         return null;
     }
-
-    public ResponseEntity<Void> deleteTeam(int teamId) {
-        return null;
-    }
-
+    @GetMapping("/team/fromArrayList")
     public ResponseEntity<List<Team>> getAllTeamsFromArrayList() {
         return null;
     }
-
-    public ResponseEntity<Integer> addTeamToArrayList(Team team) {
+   @PostMapping("/team/toArrayList")
+    public ResponseEntity<Integer> addTeamToArrayList(@RequestBody Team team) {
         return null;
     }
-
+    @GetMapping("/teamId/fromArrayList/sorted")
     public ResponseEntity<List<Team>> getAllTeamsSortedByNameFromArrayList() {
         return null;
     }
