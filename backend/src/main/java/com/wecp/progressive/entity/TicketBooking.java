eda4a7594bd30c371c3b92ae2d5303a0ca4c13c3
username <wecp-dummy@gmail.com>
2025-01-08 11:49:58 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/entity/TicketBooking.java
+++ backend/src/main/java/com/wecp/progressive/entity/TicketBooking.java
@@ -1,5 +1,41 @@
 package com.wecp.progressive.entity;
 
 public class TicketBooking {
+private int bookingId;
+private String email;
+private Match match;
+private int numberOfTickets;
+public TicketBooking(int bookingId, String email, Match match, int numberOfTickets) {
+    this.bookingId = bookingId;
+    this.email = email;
+    this.match = match;
+    this.numberOfTickets = numberOfTickets;
+}
+public TicketBooking() {
+}
+public int getBookingId() {
+    return bookingId;
+}
+public void setBookingId(int bookingId) {
+    this.bookingId = bookingId;
+}
+public String getEmail() {
+    return email;
+}
+public void setEmail(String email) {
+    this.email = email;
+}
+public Match getMatch() {
+    return match;
+}
+public void setMatch(Match match) {
+    this.match = match;
+}
+public int getNumberOfTickets() {
+    return numberOfTickets;
+}
+public void setNumberOfTickets(int numberOfTickets) {
+    this.numberOfTickets = numberOfTickets;
+}
 
 }
