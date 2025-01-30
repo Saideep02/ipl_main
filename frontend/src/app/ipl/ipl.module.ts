1b531e58bb56c2801f34e2febfb96919938bda1f
username <wecp-dummy@gmail.com>
2025-01-30 16:49:21 +0000

    wecp initial commit

--- frontend/src/app/ipl/ipl.module.ts
+++ frontend/src/app/ipl/ipl.module.ts
@@ -6,11 +6,17 @@ import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { TeamCreateComponent } from "./components/teamcreate/teamcreate.component";
 import { CricketerCreateComponent } from "./components/cricketercreate/cricketercreate.component";
+import { MatchCreateComponent } from "./components/matchcreate/matchcreate.component";
+import { TicketBookingComponent } from "./components/ticketbooking/ticketbooking.component";
+import { VoteComponent } from "./components/vote/vote.component";
 
 @NgModule({
   declarations: [
     TeamCreateComponent,
-    CricketerCreateComponent
+    CricketerCreateComponent,
+    MatchCreateComponent,
+    TicketBookingComponent,
+    VoteComponent
   ],
   imports: [
     CommonModule,
